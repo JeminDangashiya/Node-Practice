236adcc7a2a2d62967f8c9a69b3b714320e4cc4d
JeminDangashiya <jemindangashiya90728@gmail.com>
2021-07-16 08:32:37 +0530

    view engine added

---
+++ express_view.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var es6Renderer = require('express-es6-template-engine')
+
+// view engine setup
+app.engine('html', es6Renderer);
+app.set('views', 'components');
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+    res.send('<a href="/registration">REGISTRATION</a> <a href="/login">LOGIN</a>');
+})
+
+app.get('/registration', function (req, res) {
+    var arrayData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    res.render('registration', {
+        locals: {
+            title: 'Welcome!',
+            data: arrayData
+        }
+    });
+})
+
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + '/' + "login.html");
+})
+
+app.get('/form', function (req, res) {
+    res.sendFile(__dirname + '/' + "form.html");
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+})
